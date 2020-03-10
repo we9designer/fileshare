42316012ed5d022fcb1d53a69e304e1c2e60f3ee
Kamran Sadikhov <kamransadixov@mail.ru>
2020-03-10 10:03:10 +0400

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 
 class userProfile(models.Model):
     user = models.ForeignKey(User, verbose_name='UserProfile', related_name='%(class)s_requests_created', on_delete=models.CASCADE)
